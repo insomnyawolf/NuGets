98412fa59e21e68d0314a1a179c3c1f57453db6f
insomnyawolf <insomnyawolf@gmail.com>
2022-07-02 01:17:10 +0200

    Create OsExtensions File

---
+++ Extensions/Os.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Extensions
+{
+    public static class Os
+    {
+        public static void OpenInBrowser(this Uri Uri)
+        {
+            var url = Uri.ToString();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                Process.Start(url);
+            }
+        }
+
+        public static void ForceKillAsync(this Process process)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Process.Start(new ProcessStartInfo("Taskkill", $"/T /F /PID {process.Id}") { UseShellExecute = false });
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                Process.Start(new ProcessStartInfo("kill", $"-s SIGKILL {process.Id}") { UseShellExecute = false });
+            }
+            else
+            {
+                // Unsupported
+                throw new Exception("Unsupportet platform (yet)");
+            }
+        }
+    }
+}
