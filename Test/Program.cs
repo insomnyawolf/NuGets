cfe30c0e6b7d7b4588a3c944b062a7ba3dcb0342
insomnyawolf <insomnyawolf@gmail.com>
2022-07-02 18:06:17 +0200

    Booru Api Tests

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using Extensions;
 using SaucenaoSearch;
+using BooruApi;
 
 namespace Test
 {
@@ -9,21 +10,43 @@ namespace Test
     {
         public static async Task Main(string[] args)
         {
-            var saucenao = new SaucenaoWebInterface();
+            //var saucenao = new SaucenaoWebInterface();
 
-            var data = await saucenao.GetSauceAsync("https://media.discordapp.net/attachments/639815892565229579/928656817511342090/4574.jpg?width=1141&height=671");
+            //var data = await saucenao.GetSauceAsync("https://media.discordapp.net/attachments/639815892565229579/928656817511342090/4574.jpg?width=1141&height=671");
 
-            foreach (var item in data)
+            //foreach (var item in data)
+            //{
+            //    if (!string.IsNullOrEmpty(item.SourceUrl))
+            //    {
+            //        var uri = new Uri(item.SourceUrl);
+            //        uri.OpenInBrowser();
+            //        return;
+            //    }
+            //}
+
+            var booru = new BooruApiRequester(BooruServer.Gelbooru);
+
+            var autocomplete = await booru.GetAutoComplete("vermei");
+
+            var posts = await booru.GetPosts("vermeil_(arknights)");
+
+            var requestHelper = new GelbooruPostQueryHelper()
             {
-                if (!string.IsNullOrEmpty(item.SourceUrl))
+                Tags = new System.Collections.Generic.List<string>()
                 {
-                    var uri = new Uri(item.SourceUrl);
-                    uri.OpenInBrowser();
-                    return;
-                }
-            }
-        }
+                    "vermeil_(arknights)"
+                },
+                Limit = 1,
+                Page = 0,
+                Random = true,
+                Rating = BooruPostRating.Safe,
+            };
 
-        
+            var posts1 = await booru.GetPosts(requestHelper);
+
+            var posts2 = await booru.GetPosts(requestHelper);
+
+            var posts3 = await booru.GetPosts(requestHelper);
+        }
     }
 }
